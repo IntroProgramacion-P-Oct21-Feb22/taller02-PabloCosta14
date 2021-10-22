5b126df3558eaef26f5b17a7fd4df66d901f9026
Pablo Costa <pacosta2@utpl.edu.ec>
2021-10-22 11:21:50 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author DELL
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo basico De Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+    }
+
+}
